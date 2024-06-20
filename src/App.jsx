bd689b2aef11224a34703523057903157b2a705e
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-06-20 16:18:52 +0530

    feat:- design the router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './component/Home/Home'
+import Details from './component/Details/Details'
 
 const App = () => {
   return (
-    <div>
-      <h2>Movie Search</h2>
-    </div>
+   <BrowserRouter>
+   <Routes>
+   <Route path='/' element={ <Home /> } ></Route>
+   <Route path='/movie/:id' element={ <Details /> } ></Route>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
