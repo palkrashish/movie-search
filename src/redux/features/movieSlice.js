cb169d0e2109718b4984e8ac5d4abfe43141756b
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-06-20 16:21:04 +0530

    update redux

---
+++ src/redux/features/movieSlice.js
@@ -0,0 +1,43 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { BASE_API_URL } from '../../utils/constants'
+export const getMovies = createAsyncThunk("movies", async () => {
+  try {
+    const { data } = await axios.get(
+      '/movies.json'
+      // `${BASE_API_URL}/search/movie?query=batman&api_key=${
+      //   import.meta.env.VITE_MOVIE_API_KEY
+      // }`
+    );
+    return data.results;
+  } catch (error) {
+    console.log("error",error)
+  }
+});
+
+const movieSlice = createSlice({
+  name: "movies",
+  initialState: {
+    isLoading: false,
+    errorMsg: "",
+    data: [],
+  },
+
+  extraReducers: (builder) => {
+    builder.addCase(getMovies.pending, (state) => {
+      state.isLoading = true;
+      state.errorMsg = "";
+    }),
+      builder.addCase(getMovies.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.errorMsg = "";
+        state.data = action.payload;
+      }),
+      builder.addCase(getMovies.rejected, (state) => {
+        state.isLoading = false;
+        state.errorMsg ="Error while getting list of movies... Kindly try again later";
+      });
+  },
+});
+
+export default movieSlice.reducer;
